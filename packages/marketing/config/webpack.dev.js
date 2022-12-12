b0bff25ecf3d2c6c08255e3233fc066e6beff8cf
dbali00 <dbali00@bhnetwork.com>
2022-12-12 19:09:10 +0530

    add header

--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -20,9 +20,6 @@ const devConfig = {
             // shared: ['react', 'react-dom']
             shared: packageJson.dependencies
         }),
-        new HtmlWebpackPlugin({
-            template: './public/index.html'
-        })
     ]
 }
 
